17049e5d3a6bf5de989cbfd023aaaa8c81db826d
Ross Kaffenberger <rosskaff@gmail.com>
2019-11-25 07:10:25 -0500

    Display rails info and webpacker versions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def npm_webpacker_version
+    node_module_package_json("@rails/webpacker")['version']
+  end
+
+  def node_module_package_json(name)
+    JSON.parse(File.read(Rails.root.join('node_modules', name, 'package.json')))
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,8 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+require "webpacker/version"
+
 module WebpackerBootstrapDemo
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
